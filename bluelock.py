278535cfc23898d4590adbcb4934398dbb4ecb4e
aidan073 <aidanbell073@gmail.com>
2023-01-20 17:25:19 -0500

    Update bluelock.py

--- bluelock.py
+++ bluelock.py
@@ -15,11 +15,21 @@ while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        #goals
         pygame.draw.rect(screen, (255, 255, 255), (150, 0, 100, 25), )
         pygame.draw.rect(screen, (255, 255, 255), (150, 625, 100, 25), )
-        pygame.draw.rect(screen, (255, 255, 255), (0, 25, 400, 600), 2)
+
+        #field outline
+        pygame.draw.rect(screen, (255, 255, 255), (0, 25, 400, 600), 1)
+
+        #gray corners
         pygame.draw.rect(screen, (172, 172, 172), (0, 0, 150, 25), )
         pygame.draw.rect(screen, (172, 172, 172), (250, 0, 150, 25), )
         pygame.draw.rect(screen, (172, 172, 172), (0, 625, 150, 25), )
         pygame.draw.rect(screen, (172, 172, 172), (250, 625, 150, 25), )
+
+        #black goal outlines
+        pygame.draw.lines(screen, (0, 0, 0), False, [(150, 0), (150, 25), (250, 25), (250, 0)], 2)
+        pygame.draw.lines(screen, (0, 0, 0), False, [(150, 649), (150, 624), (250, 624), (250, 649)], 2)
+
         pygame.display.update()
