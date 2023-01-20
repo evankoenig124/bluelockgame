735f9c0da5244213b7dc040ccdb612ff84071dbc
aidan073 <aidanbell073@gmail.com>
2023-01-20 16:23:04 -0500

    Update bluelock.py

--- bluelock.py
+++ bluelock.py
@@ -2,10 +2,10 @@ import random
 import pygame
 import sys
 
-done = False
 width = 600
 height = 400
 grass_color = (121,222,131)
+done = False
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -14,6 +14,6 @@ screen.fill(grass_color)
 while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+            done = True
+    for x in range(0,)
+        pygame.display.update()
