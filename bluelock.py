6720e23c355618222d2d1b416c3bca4727c3bba9
aidan073 <aidanbell073@gmail.com>
2023-01-20 16:56:28 -0500

    Update bluelock.py

--- bluelock.py
+++ bluelock.py
@@ -3,7 +3,7 @@ import pygame
 import sys
 
 width = 400
-height = 700
+height = 650
 grass_color = (121,222,131)
 done = False
 
@@ -15,7 +15,11 @@ while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        pygame.draw.rect(screen, (255, 255, 255), (100, 0, 200, 50), )
-        pygame.draw.rect(screen, (255, 255, 255), (100, 650, 200, 50), )
-        pygame.draw.rect(screen, (255, 255, 255), (0, 50, 400, 600), 2)
+        pygame.draw.rect(screen, (255, 255, 255), (150, 0, 100, 25), )
+        pygame.draw.rect(screen, (255, 255, 255), (150, 625, 100, 25), )
+        pygame.draw.rect(screen, (255, 255, 255), (0, 25, 400, 600), 2)
+        pygame.draw.rect(screen, (172, 172, 172), (0, 0, 150, 25), )
+        pygame.draw.rect(screen, (172, 172, 172), (250, 0, 150, 25), )
+        pygame.draw.rect(screen, (172, 172, 172), (0, 625, 150, 25), )
+        pygame.draw.rect(screen, (172, 172, 172), (250, 625, 150, 25), )
         pygame.display.update()
